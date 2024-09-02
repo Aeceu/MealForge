0985429ca7c1eab0a6e30e3481bc56c19b235013
kaneki081 <joseacebuche2@gmail.com>
2024-09-02 10:48:09 +0800

    added user authentication endpoint

--- server/src/app.py
+++ server/src/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
+from blueprints.user import user_bp
 
 load_dotenv()
 app = Flask(__name__)
@@ -10,16 +11,17 @@ jwt = JWTManager(app)
 
 # CORS Configuration
 CORS(app, supports_credentials=True)
+
 # Configuration
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 app.config['JWT_TOKEN_LOCATION'] = ['headers', 'cookies']
 app.config['JWT_COOKIE_SECURE'] = True  # Only send cookies over HTTPS
 app.config['JWT_COOKIE_SAMESITE'] = 'None'
 app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 
-# app.register_blueprint(user_bp)
+app.register_blueprint(user_bp)
 # app.register_blueprint(ingredient_bp)
 
 if __name__ == "__main__":
