3494c4ac6ba024970f9f65baec51cc1e2e8087e8
kaneki081 <joseacebuche2@gmail.com>
2024-09-15 22:44:53 +0800

    auth endpoints done

--- client/redux/actions/userActions.ts
+++ client/redux/actions/userActions.ts
@@ -1,15 +1,32 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../api/axios";
+import { handleError } from "@/utils/errorHandler";
+import { TUserLogin, TUserSignup } from "@/utils/types/user";
 
 export const handleLogin = createAsyncThunk(
 	"user/handleLogin",
-	async (data, { rejectWithValue }) => {
+	async (data: TUserLogin, { rejectWithValue }) => {
 		try {
 			const res = await axios.post("/login", data);
 			return res.data;
 		} catch (error) {
-			console.log(error);
-			return rejectWithValue(error);
+			const resError = handleError(error);
+			console.log(resError);
+			return rejectWithValue(resError);
+		}
+	}
+);
+
+export const handleSignup = createAsyncThunk(
+	"user/handleSignup",
+	async (data: TUserSignup, { rejectWithValue }) => {
+		try {
+			const res = await axios.post("/signup", data);
+			return res.data;
+		} catch (error) {
+			const resError = handleError(error);
+			console.log(resError);
+			return rejectWithValue(resError);
 		}
 	}
 );
